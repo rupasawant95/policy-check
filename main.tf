021434733e5b1bb10b220de262653984dd5c5bd7
Rupali Sawant <101975292+rupasawant95@users.noreply.github.com>
2024-07-30 19:33:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 provider "aws" {
-    region  = "us-west-2"
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
+    region = "us-west-2"
 }
-
 resource "aws_instance" "terr-inc" {
     ami         = "ami-074be47313f84fa38"
     instance_type = "t2-micro"
