099c65d30a817e68a466c8f7a3904bdde546914c
Rupali Sawant <101975292+rupasawant95@users.noreply.github.com>
2024-07-30 20:29:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 resource "aws_instance" "terr-inc" {
     ami         = "ami-074be47313f84fa38"
-    instance_type = "t2-micro"
+    
     tags = {
     Name = "Sentinel-check"
     }
