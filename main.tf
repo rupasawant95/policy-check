b5b24ea05512c0a9cbbcc11aeb7e7121d26bf53c
Rupali Sawant <101975292+rupasawant95@users.noreply.github.com>
2024-07-30 19:35:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-    access_key = var.aws_access_key
-    secret_key = var.aws_secret_key
+    access_key = ACCESS_KEY_ID
+    secret_key = SECRET_ACEESS_KEY
     region = "us-west-2"
 }
 resource "aws_instance" "terr-inc" {
