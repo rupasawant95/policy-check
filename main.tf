d1b1dab9892166eb09c07f772cb9f5268ca39c27
Rupali Sawant <101975292+rupasawant95@users.noreply.github.com>
2024-07-31 10:17:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,27 +4,30 @@ provider "aws" {
 resource "aws_instance" "terr-inc" {
     ami         = "ami-074be47313f84fa38"
     instance_type = "t2-medium"
+    security_groups = [aws_security_group.terr-inc.name]
     tags = {
     Name = "Sentinel-check"
     }
 }
-resource "aws_security_group" "sg-group" {
-  vpc_id = aws_vpc.my_vpc.id
-  ingress {
-    from_port    = 22
-    to_port      = 22
-    protocol     = "tcp"
-    cidr_blocks  = ["0.0.0.0/0"]
-  }
+resource "aws_security_group" "terr-inc" {
+  name        = "terr-sg"
+  description = "security group"
 
   ingress {
-    from_port    = 8200
-    to_port      = 8200
-    protocol     = "tcp"
-    cidr_blocks  = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-   tags = {
-    Name = "sg-group"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terr-security-group"
   }
 }
-
