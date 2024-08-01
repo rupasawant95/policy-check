6340c9ebec2c6b09237bfe52ecd3ab79ed14febc
Rupali Sawant <101975292+rupasawant95@users.noreply.github.com>
2024-08-01 13:21:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,31 +3,27 @@ provider "aws" {
 }
 resource "aws_instance" "terr-inc" {
     ami         = "ami-074be47313f84fa38"
-    instance_type = "t2-medium"
-    security_groups = [aws_security_group.terr-inc.name]
+    instance_type = "t2-micro"
+    security_groups = [aws_security_group_rule.terr-inc.name]
     tags = {
     Name = "Sentinel-check"
     }
 }
-resource "aws_security_group" "terr-inc" {
-  name        = "terr-sg"
-  description = "security group"
+provider "aws" {
+  region = "us-west-2"
+}
 
+resource "aws_security_group_rule" "example" {
+  name        = "terra_security_group"
+  description = "Example security group"
+
+  # Define inbound rules
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "terr-security-group"
-  }
+  # Define outbound rules
 }
