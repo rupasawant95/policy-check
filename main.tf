a8c87451640028dc61bc263eceeed32fb69ccab3
Rupali Sawant <101975292+rupasawant95@users.noreply.github.com>
2024-08-02 13:52:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ resource "aws_security_group" "terr-inc" {
   description = "Example security group"
 
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/32"]
   }
 
   tags = {
