e22797d610ea7c266e7132d0fd4cb51326fbf171
Rupali Sawant <101975292+rupasawant95@users.noreply.github.com>
2024-08-02 09:51:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,18 +2,6 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_security_group" "terr-inc" {
-  name        = "terra_security_group"
-  description = "Example security group"
-
-  # Define inbound rules
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
 resource "aws_instance" "terr-inc" {
   ami             = "ami-074be47313f84fa38"
