4c5c54d89b23462359dafd43b99cf9fea2542794
Rupali Sawant <101975292+rupasawant95@users.noreply.github.com>
2024-08-02 10:09:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,6 @@ provider "aws" {
 resource "aws_instance" "terr-inc" {
   ami             = "ami-074be47313f84fa38"
   instance_type   = "t2.micro"
-  security_groups = [aws_security_group.terr-inc.name]
   tags = {
     Name = "Sentinel-check"
   }
