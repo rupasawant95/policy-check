4a7c78950ddd861416d4332a37b0bd0521c8c2f5
Rupali Sawant <101975292+rupasawant95@users.noreply.github.com>
2024-08-06 11:41:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_security_group" "terr-inc" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
     Name = "terra_security_group"
