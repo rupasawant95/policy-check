28588a3290e26e5ef55cde7107ec5a9cb5036268
Rupali Sawant <101975292+rupasawant95@users.noreply.github.com>
2024-08-12 11:17:24 +0530

    Delete continuos-validation.tf

--- continuos-validation.tf
+++
@@ -1,9 +0,0 @@
-continuous_validation_check = rule {
-    all ec2_instances as _, instance {
-        instance.change.after.instance_type is "t2.small" or instance.change.after.instance_type is "t2.medium"
-    }
-}
-
-main = rule {
-    continuous_validation_check
-}
